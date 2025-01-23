3c05bbe8f317a94dcf7b5934851c4bcc8dff2754
Vincent <vincent.ridder@tuhh.de>
2025-01-23 22:18:24 +0100

    adding logo

---
+++ lib/widgets/loading_screen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+class LoadingScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: SvgPicture.asset(
+            'lib/assets/Sperrstunde_Logo-Schriftzug_RGB.svg',
+            height: 200,
+            color: Color.fromARGB(255, 255, 95, 31)),
+      ),
+    );
+  }
+}
